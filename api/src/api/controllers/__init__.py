e92b0f57f1189ee0865af9feb88f13cd83982e5e
Szymon Szyszkowski <69353402+project-defiant@users.noreply.github.com>
2025-09-15 14:57:20 +0100

    chore: update async

--- api/src/api/controllers/__init__.py
+++ api/src/api/controllers/__init__.py
@@ -5,23 +5,20 @@ router = APIRouter(tags=["status"])
 
 
 @router.post("/submit/")
-def submit() -> dict:
-    connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-    channel = connection.channel()
-
+async def submit() -> dict:
     return {"status": "ok"}
 
 
 @router.get("/status/{uuid}")
-def status() -> dict:
+async def status() -> dict:
     return {"status": "ok"}
 
 
 @router.get("/results/{uuid}")
-def results(uuid: str) -> dict:
+async def results(uuid: str) -> dict:
     return {"status": "ok", "uuid": uuid}
 
 
 @router.get("/")
-def healthcheck():
+async def healthcheck():
     return {"status": "ok"}
