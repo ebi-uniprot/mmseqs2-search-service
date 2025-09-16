47905d8d169aa06bad27e33cc7301cc46d8b9fb5
Daniel Rice <dlrice@ebi.ac.uk>
2025-09-16 13:56:56 +0100

    Add job creation type and use in POST.

--- meta-database/main.py
+++ meta-database/main.py
@@ -4,6 +4,8 @@ from typing import Union, Annotated
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.concurrency import asynccontextmanager
 from sqlmodel import Field, Session, SQLModel, create_engine
+from pydantic import BaseModel
+
 
 class Job(SQLModel, table=True):
     job_id: str = Field(primary_key=True, index=True)
@@ -12,22 +14,27 @@ class Job(SQLModel, table=True):
     completed_at: Union[str, None] = None
     # data: Union[object, None] = Field(default=None)
 
+
 sqlite_file_name = "jobs.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(sqlite_url, connect_args=connect_args)
 
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
 
+
 SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -35,20 +42,27 @@ async def lifespan(app: FastAPI):
     yield
 
 
+class JobCreate(BaseModel):
+    job_id: str
+
+
 @app.post("/job/")
-def create_job(job_id: str, session: SessionDep):
+async def create_job(job: JobCreate, session: SessionDep):
+    job_id = job.job_id
+    print("Creating job with ID:", job_id)
     job = Job(job_id=job_id, status="QUEUED", submitted_at=datetime.datetime.now())
     session.add(job)
     session.commit()
     session.refresh(job)
     return {}
 
+
 @app.patch("/job/{job_id}")
 def update_job(job_id: str, job: Job, session: SessionDep) -> Job:
     job_db = session.get(Job, job_id)
     if not job_db:
         raise HTTPException(status_code=404, detail="Job not found")
-    job_data = job.model_dump(exclude_unset=True) 
+    job_data = job.model_dump(exclude_unset=True)
     if job_data.get("status") == "FINISHED":
         job_data["completed_at"] = datetime.datetime.now()
     job_db.sqlmodel_update(job_data)
@@ -56,6 +70,7 @@ def update_job(job_id: str, job: Job, session: SessionDep) -> Job:
     session.commit()
     session.refresh(job_db)
     return {}
+
 
 @app.get("/job/{job_id}")
 def retrieve_job(job_id: str, session: SessionDep) -> Job:
