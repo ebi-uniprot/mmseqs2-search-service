8397723cbaff6dd5eee12a71c336be7f4e0583e6
Daniel Rice <dlrice@ebi.ac.uk>
2025-09-17 09:52:09 +0100

    Formatting.

--- worker/mmseqs_service.py
+++ worker/mmseqs_service.py
@@ -4,6 +4,7 @@ import logging
 import tempfile
 import shutil
 
+
 class MMSeqsService(object):
     def __init__(self, db_dir, workspace_dir, result_dir):
         """Initialize paths for MMseqs2 service.
@@ -22,7 +23,7 @@ class MMSeqsService(object):
 
     def mmseqs2_search(self, job):
         """Run mmseqs easy-search on a FASTA sequence from the job."""
-        
+
         job_id, fasta_content = self.extract_job_id_fasta(job)
 
         logging.info(f"Processing job_id: {job_id}")
@@ -43,42 +44,39 @@ class MMSeqsService(object):
             except subprocess.CalledProcessError as e:
                 logging.error(f"mmseqs easy-search failed: {e.stderr.decode()}")
                 raise RuntimeError(f"mmseqs easy-search failed: {e.stderr.decode()}")
-            
+
             # Move the result to results folder
             final_result_file = self.result_path / f"{job_id}.m8"
             shutil.move(str(result_file), final_result_file)
             logging.info(f"Result saved to {final_result_file}")
 
-    
     def extract_job_id_fasta(self, job):
         job_id = job.get("job_id")
         if not job_id:
             raise ValueError("Job must contain a job_id")
-        
+
         logging.info(f"Got job with job_id: {job_id}")
-        
+
         fasta_content = job.get("fasta")
 
         if not fasta_content:
             raise ValueError("No FASTA content in job")
-        
-        logging.info(f"FASTA content length: {len(fasta_content)} characters")
-        return job_id,fasta_content
-    
 
-    
+        logging.info(f"FASTA content length: {len(fasta_content)} characters")
+        return job_id, fasta_content
+
     def prepare_mmseqs_cmd(self, result_file, temp_dir, query_file):
         # This will be created and populated by mmseqs
         mmseqs_tmp_dir = temp_dir / "tmp"
 
-            # build mmseqs easy-search command
+        # build mmseqs easy-search command
         cmd = [
-                "mmseqs",
-                "easy-search",
-                str(query_file),
-                str(self.db_path),
-                str(result_file),
-                str(mmseqs_tmp_dir),
-            ]
-        
+            "mmseqs",
+            "easy-search",
+            str(query_file),
+            str(self.db_path),
+            str(result_file),
+            str(mmseqs_tmp_dir),
+        ]
+
         return cmd
