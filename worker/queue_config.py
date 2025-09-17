810f4dbb9d36ceefdd1262978e843f20490eff80
Daniel Rice <dlrice@ebi.ac.uk>
2025-09-17 13:53:05 +0100

    Update queue config.

--- worker/queue_config.py
+++ worker/queue_config.py
@@ -1,5 +1,5 @@
 # Default RabbitMQ configuration
-RABBITMQ_HOST = "my-release-rabbitmq"
+RABBITMQ_HOST = "mmseqs2-queue-rabbitmq"
 RABBITMQ_PORT = 5672
 QUEUE_NAME = "task_queue"
 USER_NAME = "user"
